f083e4e5797e3b94492e79788a5d76feeaba0837
Mārtiņš Ketners <ketners@gmail.com>
2024-06-21 00:32:39 +0300

    WIP

--- ACMD1/main.c
+++ ACMD1/main.c
@@ -4,13 +4,27 @@
 
 #define BLOCKSIZE 8
 
+void block_encrypt(const unsigned char *input, unsigned char *output);
+
 int main(int argc, char *argv[])
 {
 	int n;
-	char buf[BLOCKSIZE];
+	unsigned char buf[BLOCKSIZE],
+		chiper[BLOCKSIZE];
 
 	while ((n = read(0, buf, BLOCKSIZE)) > 0) {
-		write(1, buf, n);
+		if (n == 8) {
+			block_encrypt(buf, chiper);
+			write(1, chiper, n);
+		} else {
+			write(1, buf, n);
+		}
 	}
+}
+
+void block_encrypt(const unsigned char *input, unsigned char *output)
+{
+	for (int i = 0; i < BLOCKSIZE; i++)
+		output[i] = input[BLOCKSIZE - 1 - i];
 }
 
