1b9820646767af89ef563ef43eb650dcbb9cf80d
Mārtiņš Ketners <ketners@gmail.com>
2024-06-24 23:51:56 +0300

    Revise code structure, use separate cases to improve clarity

--- ACMD1/main.c
+++ ACMD1/main.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
+#include "mode.h"
 
 #define KEYSIZE 8
 #define KEYBITS KEYSIZE*8
@@ -11,8 +12,6 @@
 #define BLOCKBITS BLOCKSIZE*8
 #define EDFLAG_ENCRYPT 0
 #define EDFLAG_DECRYPT 1
-#define OP_ENCRYPT 0
-#define OP_DECRYPT 1
 
 typedef struct bytebits {
 	unsigned int bit7 : 1;
@@ -31,31 +30,32 @@ void chain(char buf[], const char cph[], int n);
 
 int main(int argc, char *argv[])
 {
-	int i, n;
+	int i, n, flag;
 
-	char plain[BLOCKSIZE], cipher[BLOCKSIZE], plain1[BLOCKSIZE], cipher1[BLOCKSIZE];
+	char plain[BLOCKSIZE], cipher[BLOCKSIZE], plain_prev[BLOCKSIZE], cipher_prev[BLOCKSIZE];
 	char keybits[KEYBITS];
 	char blockbits[BLOCKBITS];
-
 	char *key;
-	int op = OP_ENCRYPT, nopad = 1;
-	int firstblock;
+	int mode = MODE_CBC + MODE_ENCRYPT + MODE_NOPAD + MODE_NOPAD_XOR;
 
 	// Get options
 	int ch;
-	while ((ch = getopt(argc, argv, "dek:p")) != -1) {
+	while ((ch = getopt(argc, argv, "dek:ps")) != -1) {
 		switch (ch) {
-			case 'd':
-				op = OP_DECRYPT;
+			case 'd': // decrypt
+				mode = mode | MODE_DECRYPT;
 				break;
-			case 'e':
-				op = OP_ENCRYPT;
+			case 'e': // encrypt
+				mode = mode & ~MODE_DECRYPT;
 				break;
-			case 'k':
+			case 'k': // key value
 				key = optarg;
 				break;
-			case 'p':
-				nopad = 0;
+			case 'p': // pad
+				mode = mode & ~MODE_NOPAD | MODE_PAD;
+				break;
+			case 's': // nopad with stealing
+				mode = mode & ~MODE_PAD | MODE_NOPAD_STEAL;
 				break;
 		}
 	}
@@ -71,76 +71,97 @@ int main(int argc, char *argv[])
 	printf("\n");
 #endif
 
-	switch (op) {
-		case OP_DECRYPT:
-			// here 'cipher', 'plain' is the current read ciphertext block / decrypted plaintext block, 'cipher1', 'plain1' is the previous block
-			memset(cipher1, 0, BLOCKSIZE);								// For the first block use 0 in place of the previous block value
-			if (!nopad) firstblock = 1;
-			while ((n = read(0, cipher, BLOCKSIZE)) > 0) {
-				if (n == BLOCKSIZE) {
-					// CBC full block chaining - P_i = C_i-1 ^ D_k(C_i)
-					// DES decrypt the current ciphertext block and XOR with the previous ciphertext block
-					getbits(cipher, blockbits, n);
-					encrypt(blockbits, EDFLAG_DECRYPT);
-					getbytes(blockbits, plain, n);						// Actually, it is not yet a plaintext, the plaintext is retrieved in the next step
-					chain(plain, cipher1, n);							// Here 'plain' contains the plaintext
-					memcpy(cipher1, cipher, n);
-					if (nopad) {
-						write(1, plain, n);
-					} else {
-						// Padding in use, the current block might be the last and contain padding bytes. The previous block is certain to contain no padding bytes
-						if (!firstblock) {
-							write(1, plain1, n);
-						} else {
-							firstblock = 0;
-						}
-						memcpy(plain1, plain, n);
-					}
-				} else {
-					// nopadding - no stealing (xor last part)
-					// encrypt the last ciphertext again, and xor the leftmost bits with the buffer
-					getbits(cipher1, blockbits, BLOCKSIZE);
-					encrypt(blockbits, EDFLAG_ENCRYPT);
-					getbytes(blockbits, plain, n);						// Actually, it is not yet a plaintext, the plaintext is retrieved in the next step
-					chain(plain, cipher, n);							// Here 'plain' contains the plaintext
-					write(1, plain, n);
+	switch (mode) {
+		case MODE_CBC + MODE_ENCRYPT + MODE_PAD:
+			// 'cipher' is used both for the previous and current block value as it can be reused in this flow
+			memset(cipher, 0, BLOCKSIZE);								// initialization vector
+			flag = 0;													// set when padding done for the last block
+			while ((n = read(0, plain, BLOCKSIZE)) > 0 || !flag) {
+				if (n < BLOCKSIZE) {
+					// Pad using PKCS#5 (standard block padding); if (n == 0) then a full block with padding bytes
+					memset(plain + n, BLOCKSIZE - n, BLOCKSIZE - n);
+					n = BLOCKSIZE;
+					flag = 1;
 				}
-				// TODO (n != BLOCKSIZE && padding) -> Error! (invalid block size)
-			}
-			// Detect and skip padding bytes from output
-			if (!nopad) {
-				write(1, plain, BLOCKSIZE - *(plain + BLOCKSIZE - 1));
-				// TODO (plain[BLOCKSIZE-n] > BLOCKSIZE) -> Error! (invalid padding specification)
+				// CBC full block chaining: C_i = E_k(P_i ^ C_i-1)
+				// XOR the current plaintext block with the previous block ciphertext, DES encrypt the result
+				chain(plain, cipher, n);								// plain:		`P_i ^ C_i-1`
+				getbits(plain, blockbits, n);
+				encrypt(blockbits, EDFLAG_ENCRYPT);						// blockbits:	`E_k(P_i ^ C_i-1)`
+				getbytes(blockbits, cipher, n);							// cipher:		`C_i = E_k(P_i ^ C_i-1)`
+				write(1, cipher, n);
 			}
 			break;
-		case OP_ENCRYPT:
-		default:
-			// here 'cipher' is reused, the current ciphertext block also can be used as the previous block in the beginning of the next iteration
-			memset(cipher, 0, BLOCKSIZE);								// For the first block use 0 in place of the previous block value
-			while ((n = read(0, plain, BLOCKSIZE)) > 0 || !nopad) {
-				if (n == BLOCKSIZE || !nopad) {
-					// Padding necessary
-					if (n < BLOCKSIZE) {
-						// Padding with PKCS#5 (standard block padding)
-						memset(plain + n, BLOCKSIZE - n, BLOCKSIZE - n);
-						n = BLOCKSIZE;
-						nopad = 1;
-					}
-					// CBC full block chaining -- C_i = E_k(P_i ^ C_i-1)
+		case MODE_CBC + MODE_DECRYPT + MODE_PAD:
+			memset(cipher_prev, 0, BLOCKSIZE);							// initialization vector
+			flag = 0;													// set after the 1st block
+			while ((n = read(0, cipher, BLOCKSIZE)) > 0) {
+				// TODO: (n != BLOCKSIZE && padding) -> Error! (invalid block size)
+				// CBC full block chaining: P_i = C_i-1 ^ D_k(C_i)
+				// Decrypt the current ciphertext block and XOR with the previous ciphertext block to get the plaintext
+				getbits(cipher, blockbits, n);							// blockbits: `C_i`
+				encrypt(blockbits, EDFLAG_DECRYPT);
+				getbytes(blockbits, plain, n);							// plain': `D_k(C_i)`
+				chain(plain, cipher_prev, n);							// plain: `P_i = C_i-1 ^ D_k(C_i)`
+				memcpy(cipher_prev, cipher, n);
+				// It is not yet known whether the current block is not the last one containing padding bytes, thus delay output by one block
+				if (flag) {
+					write(1, plain_prev, n);
+				} else {
+					flag = 1;
+				}
+				memcpy(plain_prev, plain, n);
+			}
+			// The last block, remove padding bytes from output
+			write(1, plain, BLOCKSIZE - *(plain + BLOCKSIZE - 1));
+			// TODO: (plain[BLOCKSIZE-n] > BLOCKSIZE) -> Error! (invalid padding specification)
+			break;
+		case MODE_CBC + MODE_ENCRYPT + MODE_NOPAD + MODE_NOPAD_XOR:
+			// 'cipher' is used both for the previous and current block value as it can be reused in this flow
+			memset(cipher, 0, BLOCKSIZE);								// initialization vector
+			while ((n = read(0, plain, BLOCKSIZE)) > 0) {
+				if (n == BLOCKSIZE) {
+					// CBC full block chaining: C_i = E_k(P_i ^ C_i-1)
 					// XOR the current plaintext block with the previous block ciphertext, DES encrypt the result
-					chain(plain, cipher, n);
+					chain(plain, cipher, n);							// cipher:		`P_i ^ C_i-1`
 					getbits(plain, blockbits, n);
-					encrypt(blockbits, EDFLAG_ENCRYPT);
-					getbytes(blockbits, cipher, n);
+					encrypt(blockbits, EDFLAG_ENCRYPT);					// blockbits:	`E_k(P_i ^ C_i-1)`
+					getbytes(blockbits, cipher, n);						// cipher:		`C_i = E_k(P_i ^ C_i-1)`
 					write(1, cipher, n);
 				} else {
-					// CBC trailing block with no padding (same length) using the XOR approach - encrypt the last full block again, then XOR with the trailing bytes
-					encrypt(blockbits, EDFLAG_ENCRYPT);
-					getbytes(blockbits, cipher, n);
-					chain(cipher, plain, n);
+					// CBC no padding XOR approach - encrypt the last full block again, then XOR with the trailing bytes
+					encrypt(blockbits, EDFLAG_ENCRYPT);					// blockbits:	`E_k(C_i-1)`
+					getbytes(blockbits, cipher, n);						// cipher':		`C_i = E_k(C_i-1)`
+					chain(cipher, plain, n);							// cipher:		`C_i = P_i ^ E_k(C_i-1)`
 					write(1, cipher, n);
 				}
 			}
+			break;
+		case MODE_CBC + MODE_DECRYPT + MODE_NOPAD + MODE_NOPAD_XOR:
+			memset(cipher_prev, 0, BLOCKSIZE);							// initialization vector
+			while ((n = read(0, cipher, BLOCKSIZE)) > 0) {
+				if (n == BLOCKSIZE) {
+					// CBC full block chaining: P_i = C_i-1 ^ D_k(C_i)
+					// DES decrypt the current ciphertext block and XOR with the previous ciphertext block
+					getbits(cipher, blockbits, n);						// cipher:		`C_i`
+					encrypt(blockbits, EDFLAG_DECRYPT);
+					getbytes(blockbits, plain, n);						// plain':		`D_k(C_i)`
+					chain(plain, cipher_prev, n);						// plain:		`P_i = C_i-1 ^ D_k(C_i)`
+					memcpy(cipher_prev, cipher, n);
+					write(1, plain, n);
+				} else {
+					// CBC no padding XOR approach - encrypt the last full block again, then XOR with the trailing bytes
+					getbits(cipher_prev, blockbits, BLOCKSIZE);
+					encrypt(blockbits, EDFLAG_ENCRYPT);					// blockbits:	`E_k(C_i-1)`
+					getbytes(blockbits, plain, n);						// plain':		`E_k(C_i-1)`
+					chain(plain, cipher, n);							// plain:		`E_k(C_i-1) ^ C_i`
+					write(1, plain, n);
+				}
+			}
+			break;
+		case MODE_CBC + MODE_ENCRYPT + MODE_NOPAD + MODE_NOPAD_STEAL:
+			break;
+		case MODE_CBC + MODE_DECRYPT + MODE_NOPAD + MODE_NOPAD_STEAL:
 			break;
 	}
 }
